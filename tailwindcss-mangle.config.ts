9fda60e1f6b3973754a80a11bd4c5e8f97087047
Gian Alcantara <raphaelalcantara51@gmail.com>
2025-04-16 23:30:28 +0800

    initial commit

---
+++ tailwindcss-mangle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tailwindcss-patch';
+
+export default defineConfig({
+  patch: {
+    output: {
+      filename: '.tw-patch/tw-class-list.json',
+      loose: true,
+      removeUniversalSelector: true
+    },
+    tailwindcss: {
+      config: './tailwind.config.js',
+      cwd: process.cwd()
+    }
+  }
+});
